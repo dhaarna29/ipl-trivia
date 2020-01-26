6363c6e1c7d203afff197115f71743fa44d80993
dhaarna29 <dhaarna29@gmail.com>
2020-01-27 01:36:27 +0530

    purgecss

--- client/ipl-facts/webpack.config.js
+++ client/ipl-facts/webpack.config.js
@@ -1,21 +1,23 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.scss$/,
-          use: [
-            {
-              loader: "postcss-loader",
-              options: {
-                plugins: [require("tailwindcss")("./tailwind.config.js"),
-			purgecss({
-              content: ['./src/*.html']
-            })			
-]
-              }
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: [
+                require("tailwindcss")("./tailwind.config.js"),
+                purgecss({
+                  content: ['./src/*.html']
+                })
+              ]
             }
-          ]
-        }
-      ]
-    }
-  };
+          }
+        ]
+      }
+    ]
+  }
+};
