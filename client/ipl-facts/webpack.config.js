ade0a3bb49bdd710e026ff553ec9c34c49bd3079
dhaarna29 <dhaarna29@gmail.com>
2020-01-27 21:15:54 +0530

    brotli

--- client/ipl-facts/webpack.config.js
+++ client/ipl-facts/webpack.config.js
@@ -2,19 +2,14 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   content: ['./src/**/*.html', './src/**/*.component.ts'],
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
 });
-const CompressionPlugin = require('compression-webpack-plugin');
+const BrotliPlugin = require(`brotli-webpack-plugin`);
 
 module.exports = {
   plugins:[
-    new CompressionPlugin({
-      test: /\.(js|css|html|svg|txt|eot|otf|ttf|gif)$/,
-      filename(info){
-          let opFile= info.path.split('.'),
-          opFileType =  opFile.pop(),
-          opFileName = opFile.join('.');
-          return `${opFileName}.${opFileType}.gzip`;
-      }
-  })
+    new BrotliPlugin({
+      asset: '[fileWithoutExt].[ext].br',
+      test: /\.(js|scss|html|svg|txt|eot|otf|ttf|gif)$/
+  }),
   ],
   module: {
     rules: [
